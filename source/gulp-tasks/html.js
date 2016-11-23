299e16fb4d067dbc7262360ac454fb020441ca5e
yeffasol <yeffasol@mail.ru>
2016-11-23 14:55:42 +0300

    html modified

--- source/gulp-tasks/html.js
+++ source/gulp-tasks/html.js
@@ -2,8 +2,11 @@ var NODE_ENV = process.env.NODE_ENV || 'development';
 module.exports = function (options) {
   return this.gulp.src(options.src)
     .pipe(this.newer(options.src))
+    .pipe(this.fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
     .pipe(this.useref())
-    .pipe(this.rigger())
     .pipe(this.if(NODE_ENV === 'production',
       this.htmlmin({
           collapseWhitespace: true
