fea31292ce76684b4aac294f56d1ff27659ba32c
yeffasol <yeffasol@mail.ru>
2016-11-24 14:41:52 +0300

    Update 2016-11-24T11:41:48.463Z

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,1 @@
-var name =
-webpackJsonpname([0,1],[
-/* 0 */
-/***/ function(module, exports) {
-
-	"use strict";
-	
-	(function () {})();
-
-/***/ }
-]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9DOi9HaXRodWIvZm9pbm5pL3NvdXJjZS9zY3JpcHRzL2luZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQSxFQUFDLFlBQVcsQ0FFWCxDQUZELEkiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oKSB7XHJcblxyXG59KSgpO1xyXG5cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9DOi9HaXRodWIvZm9pbm5pL3NvdXJjZS9zY3JpcHRzL2luZGV4LmpzIl0sInNvdXJjZVJvb3QiOiIifQ==
+var name=webpackJsonpname([0,1],[function(n,a){"use strict"}]);
